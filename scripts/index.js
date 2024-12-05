0c6ae9d41d9fef5ffc5d3da64f378cb3e309de5d
Chris LaFumee <chrislafumee@gmail.com>
2024-12-04 21:41:25 -0500

    new spots updates

--- scripts/index.js
+++ scripts/index.js
@@ -45,7 +45,7 @@ const cardLinkInput = cardModal.querySelector("#add-card-link-input");
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
-const previewModalCloseButton = previewModal.querySelector(".modal__close-button-type-preview");
+const previewModalCloseButton = previewModal.querySelector(".modal__close-button_type_preview");
 
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
